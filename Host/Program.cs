92c0b0f4ae91f4af944df15bb945dd1e58f11036
Stuart Leeks <stuartle@microsoft.com>
2017-03-28 15:43:05 -0700

    Initial single assembly loading

--- Host/Program.cs
+++ Host/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Contracts;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace Host
 {
@@ -7,6 +11,17 @@ namespace Host
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string programAssemblyPath = typeof(Program).GetTypeInfo().Assembly.Location;
+            string baseAssemblyPath = Path.GetDirectoryName(programAssemblyPath);
+            string pathToAssemblyToLoad = Path.Combine(baseAssemblyPath, "SomeDependency.dll");
+            AssemblyLoadContext.Default.LoadFromAssemblyPath(pathToAssemblyToLoad);
+
+            var type = Type.GetType("SomeDependency.Foo, SomeDependency");
+
+            var foo = (IFoo)Activator.CreateInstance(type);
+
+            Console.WriteLine(foo.DoSomething());
         }
     }
 }
---
+++ SomeDependency/Foo.cs
@@ -0,0 +1,13 @@
+﻿using Contracts;
+using System;
+
+namespace SomeDependency
+{
+    public class Foo : IFoo
+    {
+        public string DoSomething()
+        {
+            return "Hello from Foo";
+        }
+    }
+}
